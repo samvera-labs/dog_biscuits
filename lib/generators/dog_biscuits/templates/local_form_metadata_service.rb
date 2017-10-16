d5e91f9e9225d33305a2e147e57d45e8c8d25c19
Julie Allinson <geekscruff@gmail.com>
2017-10-16 11:31:56 +0100

    local_form_metadata_service.rb

---
+++ lib/generators/dog_biscuits/templates/local_form_metadata_service.rb
@@ -0,0 +1,14 @@
+# Answers queries about the cardinality of each field on the form.
+class LocalFormMetadataService < Hyrax::FormMetadataService
+  # @param [Class] model_class the class of the object
+  # @param [Symbol] field the field we want to know about
+  # @return [Boolean] true if the passed in field is a multivalued field
+  def self.multiple?(model_class, field)
+    if DogBiscuits.config.singular_properties.include? field
+      false
+    else
+      # Inquire at the model level
+      super
+    end
+  end
+end
