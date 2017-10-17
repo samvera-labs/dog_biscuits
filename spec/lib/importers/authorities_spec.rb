18191801e610de89256739182cef8e2a27c8f738
Julie Allinson <julie.allinson@london.ac.uk>
2017-10-17 09:29:23 +0100

    added test for authoritiies importer; removed test for subjects authority (currently unused as conflicts with table-based importer)

--- spec/lib/importers/authorities_spec.rb
+++ spec/lib/importers/authorities_spec.rb
@@ -3,8 +3,26 @@
 require 'spec_helper'
 
 describe DogBiscuits::Importer::Authorities do
-  # TODO: write these tests
-  # create authority and check it exists
-  # create record with uri and check it exists
-  # create record without uri and check uri is correctly formatted
+  let(:instance) { described_class.new(authority_name) }
+  let(:authority_name) { 'subjects' }
+  let(:label) { 'label' }
+  let(:uri) { 'http://example.com/label' }
+
+  it "creates an entry with a uri" do
+    expect(Qa::LocalAuthorityEntry).to receive(:create).with(
+      local_authority: instance.authority,
+      label: 'label',
+      uri: 'http://example.com/label'
+    )
+    instance.create_record(label, uri)
+  end
+
+  it "creates an entry without a uri" do
+    expect(Qa::LocalAuthorityEntry).to receive(:create).with(
+      local_authority: instance.authority,
+      label: 'label',
+      uri: 'http://example.com/subjects/label'
+    )
+    instance.create_record(label)
+  end
 end
